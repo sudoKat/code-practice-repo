553b927ababbb3abfd6baf5cd8ec64e77b7c8496
sudoKat <Katya.nomad@gmail.com>
2025-07-12 11:40:27 -0400

    initial commit

---
+++ two-sum.py
@@ -0,0 +1,26 @@
+'''
+Two Sum
+Given an array of integers nums and an integer target, return the indices i and j such 
+that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy 
+the condition.
+Return the answer with the smaller index first.
+
+'''
+def twoSum(self, nums: list[int], target: int) -> list[int]:
+       
+       'Hashmap in Python id Dictionary' 
+       nums_dict = {}
+       for i in range(len(nums)):
+        diff = target - nums[i]
+        'Check if the difference already exists in the dictionary'
+        if (((diff) in nums_dict) and (i != nums_dict[diff])):
+            'The smaller index needs to be returned first'
+            if (i < nums_dict[diff]):
+                return [i, nums_dict[diff]]
+            else:
+                return [nums_dict[diff], i]
+            'Appending a new key, so that for [5,5], the code gets no error'
+        nums_dict[nums[i]] = i
+        return []
